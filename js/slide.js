57b6863f1510fb89284682ad31589bc28d18a96d
임광민 <gm.lim@codexbridge.com>
2022-11-25 11:27:11 +0900

    slide style 추가

--- js/slide.js
+++ js/slide.js
@@ -1,4 +1,3 @@
-
 var slider = {
    mainVisualSetting: {
       dots: true,
@@ -37,10 +36,75 @@ var slider = {
          },
       ]
    },
+   centerMode: {
+      dots: false,
+      infinite: true,
+      dotsClass: 'slideDot',
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      centerMode: true,
+      centerPadding: '6rem',
+      fade:false,
+      responsive: [
+         {
+            breakpoint: 1024,
+            settings: {
+               infinite: true,
+               centerPadding: '4rem',
+            }
+         },
+         {
+            breakpoint: 767,
+            settings: {
+               slidesToShow: 1,
+               centerMode: false,
+               infinite: false,
+               centerPadding: '0',
+            }
+         },
+      ]
+   },
+   navMode: function(sliderFor, sliderNav) {
+      $(sliderFor).slick({
+         slidesToShow: 1,
+         slidesToScroll: 1,
+         arrows: false,
+         fade: true,
+         asNavFor: sliderNav,
+      });
+      $(sliderNav).slick({
+         slidesToShow: 3,
+         slidesToScroll: 1,
+         asNavFor: sliderFor,
+         dots: false,
+         centerMode: true,
+         focusOnSelect: true,
+         infinite: false,
+         responsive: [
+            {
+               breakpoint: 767,
+               settings: {
+                  slidesToShow: 2,
+                  centerMode: false,
+               }
+            },
+         ]
+      });
+   },
    funcMainVisual: function(targetName) {
       $(targetName).slick(this.mainVisualSetting);
    },
    funcType01: function(targetName) {
+      console.log(window)
       $(targetName).slick(this.type01);
+   },
+   funcCenterMode: function(targetName) {
+      $(targetName).slick(this.centerMode);
+   },
+   funcNavSlider: function(sliderFor, sliderNav) {
+      return this.navMode(sliderFor, sliderNav);
    }
 };
+
+console.log()
